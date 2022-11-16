2a0d1fb2a9a863fe8cc85df1ae203a9425257998
y0shiki <63571264+y0shiki@users.noreply.github.com>
2022-11-15 22:29:11 -0800

    Adding the Key class

---
+++ Key.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, Identity
+from sqlalchemy import ForeignKey
+from orm_base import Base
+
+
+
+
+class Key(Base):
+    __tablename__ = "keys"
+    key_id = Column('key_id', Integer, Identity(start=1, cycle=True), nullable=False, primary_key=True)
+    hook_id = Column(Integer, ForeignKey('hooks.hook_id'), primary_key=False, nullable=False)
+
+
+
+    def __init__(self, hook):
+        self.hook_id = hook.hook_id
+
+
+
