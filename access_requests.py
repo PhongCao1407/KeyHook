500006f3fb1377f4828c96462a102a4c12a65800
Phong Cao <phongcao0714@gmail.com>
2022-11-18 15:05:29 -0800

    fix fk constraint in access_request

--- access_requests.py
+++ access_requests.py
@@ -1,13 +1,19 @@
-from sqlalchemy import Column, String, Identity, Integer, ForeignKey, Date
+from sqlalchemy import Column, String, Identity, Integer, ForeignKey, Date, ForeignKeyConstraint
 from sqlalchemy.orm import relationship
 from orm_base import Base
 
+from employees import Employees
+from rooms import Room
+
 class Access_request(Base):
     __tablename__ = "access_requests"
-    employee_id = Column('employee_id', Integer, ForeignKey("employees.employee_id"), Identity(start=1, cycle=True),nullable=False, primary_key=True)
-    room_number = Column('room_number', Integer, ForeignKey("rooms.number"), Identity(start=1, cycle=True),nullable=False, primary_key=True)
-    room_building_name = Column('room_building_name', String, ForeignKey("rooms.building_name"), Identity(start=1, cycle=True),nullable=False, primary_key=True)
-    requested_date = Column('requested_date', Date, Identity(start=1, cycle=True),nullable=False, primary_key=True)
+    employee_id = Column('employee_id', Integer, nullable=False, primary_key=True)
+    room_number = Column('room_number', Integer, nullable=False, primary_key=True)
+    room_building_name = Column('room_building_name', String, nullable=False, primary_key=True)
+    requested_date = Column('requested_date', Date, Identity(start=1, cycle=True), nullable=False, primary_key=True)
+
+    __tableargs__ = (ForeignKeyConstraint([employee_id, room_number, room_building_name],
+                                          [Employees.employee_id, Room.number, Room.building_name]), {})
 
     def __init__(self, employee_id: Integer, room_number: Integer, room_building_name: String, requested_date: Date):
         self.room_number = room_number
